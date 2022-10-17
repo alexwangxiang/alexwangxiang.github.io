48b63ff20556258e646ad30bd2f37ae7fd6bb303
Wang Xiang <wx--168@163.com>
2022-10-17 19:06:33 +0800

    2022-10-17

--- copyfiles.sh
+++ copyfiles.sh
@@ -3,7 +3,8 @@ sources=("/home/alexwangxiang/Inspiron14/CourseMaterial/Linear stability of RMHD
     "/home/alexwangxiang/Inspiron14/CourseMaterial/MacMahon master theorem.pdf"
     "/home/alexwangxiang/Inspiron14/MyProject/latex/4-distance problem.pdf"
     "/home/alexwangxiang/Inspiron14/MyProject/latex/perfect euler brick problem.pdf"
-    "/home/alexwangxiang/Inspiron14/CourseMaterial/CarlaSimulator.pdf")
+    "/home/alexwangxiang/Inspiron14/CourseMaterial/CarlaSimulator.pdf"
+    "/home/alexwangxiang/Inspiron14/CourseMaterial/Yang exterior product conjecture.pdf")
     # "/home/alexwangxiang/Inspiron14/CourseMaterial/alibabacontest/alibabamath2022/alibaba2022preliminaries.pdf")
 
 for file in "${sources[@]}";
